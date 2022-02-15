5b7165399633574a04c8f3f9af04ad9b33d48fb8
Clemens Neubauer <cn-tools@gmx.at>
2022-02-15 16:11:37 +0100

    add language 'DE'

---
+++ i18n/de/ext.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'white_list' => [
+        'authorized_patterns' => 'Autorisierte Muster',
+        'warning' => [
+            'not_white_listed' => '"%s" ist kein autorisiertes Muster. Kontaktieren sie ihren Administrator für mehr Informationen.',
+        ],
+    ],
+];
